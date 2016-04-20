c4b97d5cd2265b77dd269e8643552c2ab92445b6
lobov <lobov@gloria-jeans.ru>
2016-04-20 15:56:09 +0300

    mapping add function getSeparator & optimize

--- gulp/lib/mapping.js
+++ gulp/lib/mapping.js
@@ -4,9 +4,25 @@ var fs = require('fs');
 var path = require('path');
 var _ = require('lodash');
 var config = require('../config');
-var map = {};
 
 module.exports = {
+    map: {},
+
+    /**
+     * Check separator in url
+     * @param string
+     * @returns {string}
+     */
+    getSeparator: function(string) {
+        var separator = '';
+        if (string.indexOf('\\') !== -1) {
+            separator = '\\';
+        } else if (string.indexOf('/') !== -1) {
+            separator = '/';
+        }
+
+        return separator;
+    },
 
     /**
      * Add route to Map before write
@@ -14,49 +30,41 @@ module.exports = {
      */
     add: function(history, task) {
 
-        var separator = [];
+        var src = history[0];
+        var dist = history[history.length - 1];
 
-        if (history[0].indexOf('\\') !== -1) {
-            separator[0] = '\\';
-        } else if (history[0].indexOf('/') !== -1) {
-            separator[0] = '/';
-        }
-
-        if (history[history.length - 1].indexOf('\\') !== -1) {
-            separator[1] = '\\';
-        } else if (history[history.length - 1].indexOf('/') !== -1) {
-            separator[1] = '/';
-        }
-
-
-
-        var src = _.split(history[0], separator[0]);
-        var dist = _.split(history[history.length - 1], separator[1]);
-
+        src = _.split(src, this.getSeparator(src));
         src = _.dropWhile(src, function(o) { return !(o === 'src'); });
+        // drop src category from name of route
         src = _.drop(src);
         src = _.join(src, '/');
 
+        dist = _.split(dist, this.getSeparator(dist));
         dist = _.dropWhile(dist, function(o) { return !(o === 'public'); });
         dist = _.join(dist, '/');
 
-
+        // todo: for production need another way to give dist
         if (task && task in config.tasks && history[history.length - 1].indexOf(config.tasks[task].publicPath) !== -1) {
             dist = history[history.length - 1];
         }
 
         if (src && dist) {
-            map[src] = dist;
+            this.map[src] = dist;
         }
     },
+
     /**
-     * Clear mapping
+     * Clear mapping file
      * @param env
      */
     clear: function(env) {
         var url = path.join(__dirname, _.replace(config.mapping, '{env}', env));
 
-        fs.unlinkSync(url);
+        fs.access(url, fs.F_OK, function(err) {
+            if (!err) {
+                fs.unlinkSync(url);
+            }
+        });
     },
 
     /**
@@ -64,15 +72,14 @@ module.exports = {
      * @param env
      */
     write: function (env) {
+        var self = this;
         var url = path.join(__dirname, _.replace(config.mapping, '{env}', env));
 
         fs.access(url, fs.F_OK, function(err) {
             if (!err) {
-                _.merge(map, JSON.parse(fs.readFileSync(url, 'utf8')));
-                fs.writeFileSync(url, JSON.stringify(map, null, 4));
-            } else {
-                fs.writeFileSync(url, JSON.stringify(map, null, 4));
+                _.merge(self.map, JSON.parse(fs.readFileSync(url, 'utf8')));
             }
+            fs.writeFileSync(url, JSON.stringify(self.map, null, 4));
         });
     }
 };
