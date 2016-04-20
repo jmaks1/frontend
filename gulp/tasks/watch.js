6f09e9e4b77735fce6cb94627a8cdd1649a3b15a
lobov <lobov@gloria-jeans.ru>
2016-04-20 15:53:52 +0300

    watch use array of tasks

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -9,14 +9,25 @@ var taskUrl = require('../lib/createUrl');
 var taskName = path.basename(__filename, '.js');
 
 var watch = require('gulp-watch');
-var taskWatch = ['javascripts', 'spritePng', 'spriteSvg', 'icons', 'stylesheets'];
+var gutil = require('gulp-util');
 
-module.exports = function(runTimestamp) {
-    gulp.task(taskName + ((runTimestamp)?':production':''), function () {
+module.exports = function(taskWatch) {
+    gulp.task(taskName, function () {
         taskWatch.forEach(function(item, i, arr) {
             watch([taskUrl(item, 'watch')], function(event, cb) {
                 gulp.start(item);
             });
         });
+
+        // i'm watching for you!
+        var message =
+            '___/\\                         __         .__    .__                   _____                                  ._. \n' +
+            '|   )/_____   __  _  _______ _/  |_  ____ |  |__ |__| ____    ____   _/ ____\\___________   ___.__. ____  __ __| | \n' +
+            '|   |/     \\  \\ \\/ \\/ /\\__  \\\\   __\\/ ___\\|  |  \\|  |/    \\  / ___\\  \\   __\\/  _ \\_  __ \\ <   |  |/  _ \\|  |  \\ | \n' +
+            '|   |  Y Y  \\  \\     /  / __ \\|  | \\  \\___|   Y  \\  |   |  \\/ /_/  >  |  | (  <_> )  | \\/  \\___  (  <_> )  |  /\\| \n' +
+            '|___|__|_|  /   \\/\\_/  (____  /__|  \\___  >___|  /__|___|  /\\___  /   |__|  \\____/|__|     / ____|\\____/|____/ __ \n' +
+            '          \\/                \\/          \\/     \\/        \\//_____/                         \\/                  \\/\n';
+
+        gutil.log('\n\n', gutil.colors.yellow(message), '\n');
     });
 };
